8e558fe992cce528cbd0741b0c133c8d4ac9a1e2
Jared Bruni <jaredbruni@gmail.com>
2026-09-24 16:02:34 -0700

    start

--- MXParser/parser.hpp
+++ MXParser/parser.hpp
@@ -1,6 +1,26 @@
 #pragma once
 #include<string>
+#include<MXLex/scanner.hpp>
+#include<format>
+#include<print>
+#include<utility>
 
 namespace mx {
-    class Parser {};
+
+    class ParserException : public std::runtime_error  {
+    public:
+        ParserException(const std::string &msg) : runtime_error(msg) {}
+    private:
+        std::string message;
+    };
+
+    class Parser {
+    public:
+        Parser(std::istream &stream) : scanner{stream}, error_count{0} {}
+        bool parse();
+        void error_message(const std::string &message, int line);
+    protected:
+        Scanner scanner;
+        size_t error_count;
+    };
 }
--- source/parser.cpp
+++ source/parser.cpp
@@ -1,1 +1,14 @@
 #include "MXParser/parser.hpp"
+
+namespace mx {
+
+    bool Parser::parse() {
+
+
+        return true;
+    }
+
+    void Parser::error_message(const std::string &message, int line) {
+        std::println("Error: {} on Line: {}", message, line);
+    }
+}
